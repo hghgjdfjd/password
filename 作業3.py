dde082d00015be1f2d742837c50aa8f976ef4a69
Damiel <aabccdaniel@gmail.com>
2017-11-25 20:37:34 +0800

    add new file

---
+++ 作業3.py
@@ -0,0 +1,24 @@
+password = 'a123456'
+i=3
+while i != 0:
+	x = input('請輸入密碼:')
+	if x==password:
+		print('登入成功')
+		break
+	else:
+		i=i-1
+		if:
+		 i>0
+		 print('輸入錯誤還有', i, '次機會')
+		else:
+			print('輸入錯誤')
+
+
+
+
+
+
+
+
+
+
